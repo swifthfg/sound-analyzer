ffe4babd1efab96cd2c4a303e9fe6cd8e39066c6
faruk <gehafar@gmail.com>
2019-01-20 05:53:33 +0300

    Add time and frequency domain amplitude analysis

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-import mutagen
+import matplotlib.pyplot as plt
+from numpy import fft as fft
+import numpy as np
 import scipy.io.wavfile
 import pydub
 import os
 
+# to get rid of the error saying the plotlib cannot draw that much data points
+plt.rcParams['agg.path.chunksize'] = 10000
+
 EXTENSION_WAV = '.wav'
 
-def convertMP3toWAV(fileMP3Path):
-    fileName = os.path.basename(fileMP3Path)
-    wavName = os.path.splitext(fileName)[0] + EXTENSION_WAV
 
+def convertMP3toWAV(fileMP3Path):
+    baseName = os.path.basename(fileMP3Path)
+    wavName = os.path.splitext(baseName)[0] + EXTENSION_WAV
     mp3File = pydub.AudioSegment.from_mp3(fileMP3Path)
     mp3File.export(wavName, format="wav")
+
+
+def drawFastFourierTransformedSoundGraph(audioData):
+    fourier = fft.fft(audioData)
+    plt.plot(fourier, color='#ff7f00')
+    plt.xlabel('k')
+    plt.ylabel('Amplitude')
+    plt.show()
+
+
+def drawAmplitudeOverTimeGraph(rate, audioData):
+    time = np.arange(0, float(audioData.shape[0]), 1) / rate
+    plt.figure(1)
+    plt.subplot(211)
+    plt.plot(time, audioData, linewidth=0.01, alpha=0.7, color='#ff7f00')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude')
+    plt.show()
+
+
+def getLeftChannelDataOfSound(audioData):
+    # first column
+    return audioData[:, 0]
+
+
+def getRightChannelDataOfSound(audioData):
+    # second column
+    return audioData[:, 1]
+
+
+def getTrackTimeInSeconds(rate, audioData):
+    # length of the track = allDataPoints / dataPointsPerSecond
+    return audioData.shape[0] / rate
+
 
 def main():
     print('Start analyzing')
@@ -19,10 +58,12 @@ def main():
     rate, audioData = scipy.io.wavfile.read(fileWAV)
     print('Rate: ' + str(rate))  # how many data points are there per second
     print('Audio shape: ' + str(audioData.shape))
-    print('Lengt of music in seconds: ' + str(audioData.shape[0] / rate))  # length of the track = allDataPoints / dataPointsPerSecond
+    print('Length of music in seconds: ' + str(audioData.shape[0] / rate))
+    print('Number of mono/stereo channels: ' + str(audioData.shape[1]))
 
+    time = np.arange(0, float(audioData.shape[0]), 1) / rate
 
-
+    # drawAmplitudeOverTimeGraph(rate, audioData)
 
 
 if __name__ == '__main__':
